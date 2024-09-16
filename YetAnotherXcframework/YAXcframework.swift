58c0c84328f2913dbf4855b375f853379170cc93
Raffi <raffi@Raffis-MacBook-Pro.local>
2024-09-16 14:01:57 +0200

    init commit

---
+++ YetAnotherXcframework/YAXcframework.swift
@@ -0,0 +1,14 @@
+//
+//  YAXcframework.swift
+//  YetAnotherXcframework
+//
+//  Created by Raffi on 16/09/2024.
+//
+
+import Foundation
+
+public class YAXcframework {
+    public func log() {
+        print("Hello YAXcframework!")
+    }
+}
